f916883bf05f0cd073bea4c628a7cc1bb7bbcbce
Yureki3657 <109599658+Yureki3657@users.noreply.github.com>
2024-07-07 15:12:24 +0700

    Create Initi.cpp

---
+++ Initi.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll              long long
+#define el              cout << '\n'
+#define f(i,start,end)  for(long long i=start;i<=end;++i)
+#define fv(i,start,end) for(long long i=start;i>=end;i--)
+#define ft              first
+#define sd              second
+#define llPair          pair<long long,long long>
+#define ep              emplace_back
+#define pb              push_back
+using namespace std;
+const ll INF=3e18+1, N=1e3+5, MOD = 1e9+7;
+
+void Initialization();
+
+ll t, n, m, ans;
+
+int main()
+{ 
+    Initialization();
+    
+    cin >> n;
+    
+    return 0;
+}
+
+void Initialization() {
+    #define thukem ""
+    if(fopen(thukem".inp", "r")) {
+        freopen(thukem".inp","r",stdin);
+        freopen(thukem".out","w",stdout);
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+}
+
+
